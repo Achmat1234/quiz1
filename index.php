254604451072056bb494936f520f553b70da7de1
Achmat <achmat.zaydan@gmail.com>
2019-08-20 23:30:33 +0200

    added 14th question

--- index.php
+++ index.php
@@ -170,6 +170,16 @@ $note = 'Welcome to Achmats quiz.';
                     <input type="radio" name="answer-13" value="D">Ocean basket<br>
                 </div>
 
+                <div id="question-container">
+                    <li>
+                        <h2 class="question" id="14">What is the most well known fast food place in Cape Town?</h2>
+                    </li>
+                    <input type="radio" name="answer-14" value="A">McDonald's<br>
+                    <input type="radio" name="answer-14" value="B">Food-Inn<br>
+                    <input type="radio" name="answer-14" value="C">Kfc<br>
+                    <input type="radio" name="answer-14" value="D">Ocean basket<br>
+                </div>
+
 
 
 
