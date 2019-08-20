d40abc2d1707976a79f57dd029b2a277c4b36552
Achmat <achmat.zaydan@gmail.com>
2019-08-20 22:47:29 +0200

    first value can be anything as array initialise with 0

--- result.php
+++ result.php
@@ -10,3 +10,17 @@ if (!isset($_POST['btn'])) {
 
 $totalMarks = 0; // Do not edit, initialisation of variable
 $totalQues = 7; //Enter total number of questions here
+
+
+//first value can be anything as array initialise with 0
+$ans = array(
+    'undef',
+    'A',
+    'B',
+    'A',
+    'A',
+    'D',
+    'D',
+    'D',
+
+);
