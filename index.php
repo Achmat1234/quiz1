1fd99df673eb4ba9009d8110c0766e7afbf7983e
Achmat <achmat.zaydan@gmail.com>
2019-08-21 08:52:11 +0200

    added new answer to my questions

--- index.php
+++ index.php
@@ -67,34 +67,34 @@ $note = 'Welcome to Achmats quiz.';
 
                 <div id="question-container">
                     <li>
-                        <h2 class="question" id="4">Film and TV institute of India is located at</h2>
+                        <h2 class="question" id="4">What is the tallest building in Cape Town?</h2>
                     </li>
-                    <input type="radio" name="answer-4" value="A">Pune (Maharashtra)<br>
-                    <input type="radio" name="answer-4" value="B">Rajkot (Gujarat)<br>
-                    <input type="radio" name="answer-4" value="C">Pimpri (Maharashtra)<br>
-                    <input type="radio" name="answer-4" value="D">Perambur (Tamilnadu)<br>
+                    <input type="radio" name="answer-4" value="A">Absa Building<br>
+                    <input type="radio" name="answer-4" value="B">Metropolitan Building<br>
+                    <input type="radio" name="answer-4" value="C">Media 24 Building<br>
+                    <input type="radio" name="answer-4" value="D">Fnb Building<br>
                 </div>
 
 
                 <div id="question-container">
                     <li>
-                        <h2 class="question" id="5">The ozone layer restricts</h2>
+                        <h2 class="question" id="5">Oshark Marine World is in ...?</h2>
                     </li>
-                    <input type="radio" name="answer-5" value="A">Visible light<br>
-                    <input type="radio" name="answer-5" value="B">Infrared radiation<br>
-                    <input type="radio" name="answer-5" value="C">X-rays and gamma rays<br>
-                    <input type="radio" name="answer-5" value="D">Ultraviolet radiation<br>
+                    <input type="radio" name="answer-5" value="A">Durban<br>
+                    <input type="radio" name="answer-5" value="B">Pretoria<br>
+                    <input type="radio" name="answer-5" value="C">Cape Town<br>
+                    <input type="radio" name="answer-5" value="D">Bloemfontein<br>
                 </div>
 
 
                 <div id="question-container">
                     <li>
-                        <h2 class="question" id="6">The ozone layer restricts</h2>
+                        <h2 class="question" id="6">What is Cignal Heal well known for?</h2>
                     </li>
-                    <input type="radio" name="answer-6" value="A">Visible light<br>
-                    <input type="radio" name="answer-6" value="B">Infrared radiation<br>
-                    <input type="radio" name="answer-6" value="C">X-rays and gamma rays<br>
-                    <input type="radio" name="answer-6" value="D">Ultraviolet radiation<br>
+                    <input type="radio" name="answer-6" value="A">View<br>
+                    <input type="radio" name="answer-6" value="B">Trees<br>
+                    <input type="radio" name="answer-6" value="C">Lions<br>
+                    <input type="radio" name="answer-6" value="D">Braai Spots<br>
                 </div>
 
 
